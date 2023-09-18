3c28767a305e9f617a4cab65fdd193f4b5455fed
Enzo <enzo.gallone83@gmail.com>
2023-09-18 11:20:59 -0300

    ajustes feitos

--- SPA-1TDSPM/src/components/Cabecalho.jsx
+++ SPA-1TDSPM/src/components/Cabecalho.jsx
@@ -6,6 +6,7 @@ export default function Cabecalho() {
     <>
       <header>
         <h1>Vite + React / Coded By - PF0670</h1>
+        <h2>ISOLANTE - LINDO</h2>
         <ul>
           <li>
             <Link to="/">HOME - <H/></Link>
