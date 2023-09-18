b94478356c64cbbffd78a713d9a8ef48ac93c6e5
Enzo <enzo.gallone83@gmail.com>
2023-09-18 12:04:54 -0300

    exemplos de useEfffect com x3

--- SPA-1TDSPM/src/routes/Produtos.jsx
+++ SPA-1TDSPM/src/routes/Produtos.jsx
@@ -8,17 +8,35 @@ import { useEffect, useState } from "react";
 export default function Produtos() {
   document.title = "Produtos";
 
+  useEffect(()=>{
+    console.log("Este useEffect renderiza sempre! " + counter);
+  });
+  
   const [counter, setCounter] = useState(0);
 
+  const [listaProdutosLocal, setListaProdutosLocal] = useState([{}]);
+
   useEffect(()=>{
-    console.log("Este useEffect renderiza sempre!");
-  });
+    console.log("Este useEffect renderiza apenas uma vez!");
+    setListaProdutosLocal(ListaProdutos);
+  },[ ]);
+
+  const [counter2, setCounter2] = useState(0)
+
+  useEffect(()=>{
+    console.log("Este useEffect renderiza sempre que o objeto/variável ou elemento que está sendo monitorado no array de dependências sofra alguma atualização.");
+  },[counter2]);
+
+
 
   return ( 
     <div>
       <h1>LISTA DE PRODUTOS</h1>
       <div>
-        <button onClick={()=> setCounter(1)}>COUNTER - {counter}</button>
+        <button onClick={()=> setCounter(counter + 1)}>COUNTER - {counter}</button>
+      </div>
+      <div>
+        <button onClick={()=> setCounter2(counter2 + 1)}>COUNTER2 - {counter2}</button>
       </div>
       <table className={style.tblEstilo}>
         <thead>
@@ -32,7 +50,7 @@ export default function Produtos() {
         </thead>
         <tbody>
         {
-          ListaProdutos.map((item,indice)=>(
+          listaProdutosLocal.map((item,indice)=>(
             <tr key={indice} className={style.tblLine}>
                 <td>{item.id}</td>
                 <td>{item.nome}</td>
